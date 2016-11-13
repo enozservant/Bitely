b829fda5699f0d0afdde092eba9ee01714405b75
YUANXU <yuanx@usc.edu>
2016-11-12 16:43:55 -0800

    temp

---
+++ BitelyLogin/app/src/main/java/game_logic/User.java
@@ -0,0 +1,17 @@
+package game_logic;
+
+/**
+ * Created by williamxu on 12/11/2016.
+ */
+
+public class User {
+
+    private List<User> friends;
+    private List<Restaurant>
+
+
+    public User(){
+
+    }
+
+}
