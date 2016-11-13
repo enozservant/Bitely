8e668280d10a85efa2ef237036fc5588a57e9662
Soren Craig <tcraig@usc.edu>
2016-11-12 20:40:41 -0800

    Trying to pop

--- BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/LoginActivity.java
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/LoginActivity.java
@@ -7,6 +7,7 @@ package com.finalproject.bitelyapp;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -17,6 +18,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import butterknife.ButterKnife;
 import butterknife.Bind;
 
@@ -24,7 +31,13 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
-    @Bind(R.id.input_username) EditText _usernameText;
+    private boolean successfulLogin = false;
+
+    // a reference to the firebase database
+    private FirebaseAuth mFirebaseAuth;
+
+    // @Bind(R.id.input_username) EditText _usernameText;
+    @Bind(R.id.input_email) EditText _userEmail;
     @Bind(R.id.input_password) EditText _passwordText;
     @Bind(R.id.btn_login) Button _loginButton;
     @Bind(R.id.link_signup) TextView _signupLink;
@@ -34,6 +47,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
         
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
@@ -72,38 +87,34 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        String username = _usernameText.getText().toString();
-        String password = _passwordText.getText().toString();
-
         /*
          * // TODO: Implement authentication with server
          */
+        String email = _userEmail.getText().toString();
+        String password = _passwordText.getText().toString();
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
                         progressDialog.dismiss();
+                        if(task.isSuccessful())
+                        {
+                            loginSuccess();
+                            Log.d(TAG, "Login Success");
+                        }
+                        else
+                        {
+                            Log.d(TAG, "Login Failure");
+                            onLoginFailed();
+
+                        }
                     }
-                }, 3000);
-        Intent intent = new Intent(this, MainScreenLogged.class);
-        startActivity(intent);
-    }
+                });
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SIGNUP) {
-            if (resultCode == RESULT_OK) {
-
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-                // Query the db for the username/password match
-                this.finish();
-            }
-        }
     }
 
     @Override
@@ -112,16 +123,24 @@ public class LoginActivity extends AppCompatActivity {
         moveTaskToBack(true);
     }
 
-    public void onLoginSuccess() {
+    public void loginSuccess()
+    {
         _loginButton.setEnabled(true);
         finish();
+        Intent intent = new Intent(this, MainScreenLogged.class);
+        startActivity(intent);
+
     }
 
-    public void onLoginFailed() {
+    public void onLoginFailed()
+    {
+        // handle failed fields shit
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
         _loginButton.setEnabled(true);
     }
+
+
+
 
     /*
      * Validate the username and password are in proper form
@@ -129,20 +148,21 @@ public class LoginActivity extends AppCompatActivity {
     public boolean validate() {
         boolean valid = true;
 
-        String username = _usernameText.getText().toString();
+        // String username = _usernameText.getText().toString();
+        String email = _userEmail.getText().toString();
         String password = _passwordText.getText().toString();
 
         // !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
-        if (username.isEmpty()) {
-            _usernameText.setError("enter a valid username");
+        if (email.isEmpty()) {
+            _userEmail.setError("enter a valid email");
             valid = false;
         } else {
-            _usernameText.setError(null);
+            _userEmail.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 4 || password.length() > 50) {
+            _passwordText.setError("between 4 and 50 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
