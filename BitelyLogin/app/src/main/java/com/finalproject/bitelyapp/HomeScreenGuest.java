167041b44123f731ec17ab1f6e81da720e72acc2
Soren Craig <tcraig@usc.edu>
2016-11-13 16:53:49 -0800

    Clean up file

--- BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/HomeScreenGuest.java
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/HomeScreenGuest.java
@@ -53,14 +53,6 @@ public class HomeScreenGuest extends AppCompatActivity
         }
     }
 
-    /*
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    */
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
