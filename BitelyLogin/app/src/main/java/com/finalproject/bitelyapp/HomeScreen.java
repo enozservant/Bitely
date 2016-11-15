8aaba57751bd91115e5d5f707550eb11fbdb495f
mengyaoh <mengyaoh@usc.edu>
2016-11-14 20:41:21 -0800

    please push

--- BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/HomeScreen.java
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/HomeScreen.java
@@ -2,9 +2,7 @@ package com.finalproject.bitelyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,7 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class HomeScreen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -92,7 +89,7 @@ public class HomeScreen extends AppCompatActivity
         {
             // Handle the my list action
             Log.i(TAG,"BiteMeActivity in myList");
-            Intent i = new Intent(this, BiteMeActivity.class);
+            Intent i = new Intent(this, MylistActivity.class);
             this.startActivity(i);
         } else if (id == R.id.nav_trending)
         {
@@ -100,6 +97,8 @@ public class HomeScreen extends AppCompatActivity
         } else if (id == R.id.nav_my_friends)
         {
             // Handle the my my friends action
+            Intent i = new Intent(this, MyFriendActivity.class);
+            this.startActivity(i);
         } else if (id == R.id.nav_browse_screen)
         {
             // Handle the my browse screen action
