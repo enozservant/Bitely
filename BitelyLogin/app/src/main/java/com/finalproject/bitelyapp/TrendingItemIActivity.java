961bc773be437f537f6c00a7143b0c32309256b2
YUANXU <yuanx@usc.edu>
2016-11-14 22:12:04 -0800

    trending item

---
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/TrendingItemIActivity.java
@@ -0,0 +1,13 @@
+package com.finalproject.bitelyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrendingItemIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trending_item);
+    }
+}
