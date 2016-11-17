4418678db1862c7ed18ac7ae378e32e9454535aa
mengyaoh <mengyaoh@usc.edu>
2016-11-16 17:45:07 -0800

    ADD BITEME

--- BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/BiteMeActivity.java
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/BiteMeActivity.java
@@ -1,122 +1,112 @@
 package com.finalproject.bitelyapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class BiteMeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.Business;
+import com.yelp.clientlib.entities.SearchResponse;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import retrofit2.Call;
+
+public class BiteMeActivity extends AppCompatActivity {
+
+    private final String TAG = "BiteMeActivity";
+
+    ListItem biteItem;
+    private ImageView image_view;
+    private TextView headlineView;
+    private Button Biteme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bite_me);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_bite_me);
+        image_view=(ImageView) findViewById(R.id.biteMeRestaurantPic);
+        headlineView= (TextView) findViewById(R.id.rname);
+        biteItem = new ListItem();
+        callYelp("Los Angeles");
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        Biteme = (Button) findViewById(R.id.biteMeAgain);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        Biteme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callYelp("Los Angeles");
+//                Intent i = new Intent(BiteMeActivity.this, BiteMeActivity.class);
+//                startActivity(i);
+            }
+        });
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    private void initializeListView()
+    {
+        new ImageDownloaderTask(image_view).execute(biteItem.getImageURL());
+        headlineView.setText(biteItem.getName());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.bite_me, menu);
-        return true;
+
+    private void callYelp(String str){
+        final String loc = str;
+        Runnable r = new Runnable(){
+            @Override
+            public void run() {
+
+                try {
+                    YelpAPIFactory apiFactory = new YelpAPIFactory("ezGAcfxEnpiWLsbtDvHxhw","3fObU_RY_UyED5bUiixGbHuev6Q",
+                            "iXKNIgegrgUNfSPJcFzz59PUt0vjARbn", "zZdQfjNqLrujtGxwD4DJS5IcIxM");
+                    YelpAPI yelpAPI = apiFactory.createAPI();
+
+                    Map<String, String> params = new HashMap<>();
+                    params.put("term", "restaurant");
+                    params.put("limit","20");
+
+                    Call<SearchResponse> call = yelpAPI.search(loc,  params);
+                    SearchResponse response = call.execute().body();
+
+                    ArrayList<Business> businesses = response.businesses();
+
+                    Log.d("YELP", businesses.size() + " responses received.");
+
+                    populateListGUI(businesses);
+
+                } catch (IOException ioe){
+                    ioe.printStackTrace();
+                }
+            }
+        };
+
+        Thread yelpThread = new Thread (r);
+        yelpThread.start();
+
+        try
+        {
+            yelpThread.join();
+            initializeListView();
+
+        } catch(InterruptedException ie) { Log.d("YELP_THREAD", "Yelp thread interrupted"); }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home_screen)
-        {
-            // Handle the home screen action
-            //Log.i(TAG,"I'm Here");
-            Intent i = new Intent(this, HomeScreen.class);
-            this.startActivity(i);
-        } else if (id == R.id.nav_my_list)
-        {
-            // Handle the my list action
-            //Log.i(TAG,"BiteMeActivity in myList");
-            Intent i = new Intent(this, BiteMeActivity.class);
-            this.startActivity(i);
-        } else if (id == R.id.nav_trending)
-        {
-            // Handle the trending action
-        } else if (id == R.id.nav_my_friends)
-        {
-            // Handle the my my friends action
-        } else if (id == R.id.nav_browse_screen)
-        {
-            // Handle the my browse screen action
-        } else if (id == R.id.nav_user_profile)
-        {
-            // Handle the nav user profile action
-            //Log.i(TAG, "UserProfile");
-            Intent i = new Intent(this, ProfileActivity.class);
-            this.startActivity(i);
-        }
-        else if(id == R.id.nav_user_logout)
-        {
-            startActivity(new Intent(getApplicationContext(), LogoutActivity.class));
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    private void populateListGUI(ArrayList<Business> businessList){
+        Random rn = new Random();
+        int answer = rn.nextInt(businessList.size());
+        Log.i(TAG, businessList.get(answer).name());
+        Log.i(TAG, businessList.get(answer).imageUrl());
+        biteItem.setName(businessList.get(answer).name());
+        biteItem.setImageURL(businessList.get(answer).imageUrl());
+        // restaurantImagesUrl.add(businessList.get(i).imageUrl());
     }
 }
