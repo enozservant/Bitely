c65158e5b45f6011eaf78fdb44831061e699fdcd
Soren Craig <tcraig@usc.edu>
2016-11-16 17:26:50 -0800

    Fix broken things in master

--- BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/ResterautinfoAcivity.java
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/ResterautinfoAcivity.java
@@ -9,7 +9,7 @@ public class ResterautinfoAcivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.restaurant_infor);
+        setContentView(R.layout.restaurant_info);
 
     }
 
