7147ad1dbaa2525093af05b71fffb807e22618f9
Soren Craig <tcraig@usc.edu>
2016-11-16 17:45:15 -0800

    Add ability to call user

--- BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/UserInfoActivity.java
+++ BitelyLogin/app/src/main/java/com/finalproject/bitelyapp/UserInfoActivity.java
@@ -1,8 +1,13 @@
 package com.finalproject.bitelyapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +21,7 @@ public class UserInfoActivity extends AppCompatActivity {
     private TextView typeTextView;
     private TextView ratingView;
     private ImageView restaurantImage;
+    private Button callButton;
 
     private String displayReviewText;
 
@@ -26,10 +32,34 @@ public class UserInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
+
+
+       callButton = (Button) findViewById(R.id.call_user_button);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // call the friend
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + chosenUser.getPhoneNumber()));
+                /*
+                if (ActivityCompat.checkSelfPermission(UserInfoActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                */
+                startActivity(callIntent);
+            }
+        });
+
         // get the restaurant item
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         chosenUser = (User) bundle.getSerializable(USER_CHOSEN);
+
+        // hardcode the user's phone number to test
+        chosenUser.setPhoneNumer("7608518162");
 
         // bind the view variables
         nameTextView = (TextView) findViewById(R.id.person_info_name);
